f3ae81f6a7303ae6360ff19119d3282618b18a63
rolex20 <62082564+rolex20@users.noreply.github.com>
2025-12-22 03:04:06 -0600

    Release candidate versions

--- telnet-get.ps1
+++ telnet-get.ps1
@@ -1,6 +1,4 @@
-# A simple telnet like GET to troubleshoot web server JSON files
- 
- # Create a TCP client and connect to localhost:8181
+﻿# Create a TCP client and connect to localhost:8181
 $client = New-Object System.Net.Sockets.TcpClient
 $client.Connect("localhost", 8181)
 
@@ -17,14 +15,16 @@ $writer.Flush()
 # Read the raw response
 # 1. Use ReadToEnd() to get the whole response as one string
 # This avoids the "ReadLine" loop and manually adding newlines
+$response = "nada" # just a flag
 $response = $reader.ReadToEnd()
 
 # 2. Avoid Write-Output if you are just checking the raw data
 # Instead, output to a file or use a specific Width to see if it's truly broken
+
+Remove-Item -path "raw_response.json"
 $response | Set-Content "raw_response.json"
 
 # OR, if you must print to console, force it to not wrap:
-
 Write-Host $response
 
 
