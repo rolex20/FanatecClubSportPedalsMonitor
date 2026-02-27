de063e0896ce88a17ad56df630406b5a665af44c
rolex20 <ralch@live.com>
2026-02-27 01:33:50 -0600

    Moved deprecated support tools/programs to /deprecated

---
+++ deprecated/sayRudder.ps1
@@ -0,0 +1,10 @@
+﻿#Get-Location
+#$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+#Write-Output "[$timestamp] Rudder noise detected."
+
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.SelectVoiceByHints([System.Speech.Synthesis.VoiceGender]::Female, [System.Speech.Synthesis.VoiceAge]::Adult)
+$speak.Rate = 3
+#$x = $speak.SpeakAsync('Rudder.  Rudder.  Rudder.')
+$speak.Speak('Rudder')
